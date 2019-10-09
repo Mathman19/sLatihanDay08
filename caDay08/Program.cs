f1a8bf0f652a73661dfb6474e615843c0a66ca8b
Muhammad Akbar <akbarprima95@gmail.com>
2019-10-09 20:13:42 +0700

    bootcamp day 08 Xsis

---
+++ caDay08/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string huruf1 = "selamat pagi";
+                string huruf2 = "selamat siang";
+                string huruf3 = "selamat malam";
+                string huruf4 = "selamat datang";
+
+                int angka1 = 1;
+                int angka2 = 6;
+
+                int[] angka3 = { 1, 2, 3, 4, 5, 6 };
+
+                int call = hitung(angka1, angka2);
+                cetak(angka3, angka2);
+
+                Console.WriteLine(huruf1);
+                Console.WriteLine(huruf2);
+                Console.WriteLine(huruf3);
+                Console.WriteLine(huruf4);
+                Console.WriteLine(call);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+
+            Console.ReadKey();
+        }
+        public static int hitung(int a, int b)
+        {
+            int result = a + b;
+            for (int i = 0; i < b; i++)
+            {
+                Console.WriteLine("baris ke " + i);
+            }
+
+            return result;
+        }
+        public static void cetak(int[] a, int ang)
+        {
+            for (int i = 0; i < ang; i++)
+            {
+                Console.WriteLine("value array "+ i + "is" + a[i]);
+            }
+        }
+    }
+}
